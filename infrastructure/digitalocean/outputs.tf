0b730671d14c6d880a95fdf194db7ff04c12eb72
waseem99 <waseem99@users.noreply.github.com>
2026-07-30 20:21:10 +0500

    Format DigitalOcean Terraform outputs

--- infrastructure/digitalocean/outputs.tf
+++ infrastructure/digitalocean/outputs.tf
@@ -1,5 +1,19 @@
-output "droplet_id" { value = digitalocean_droplet.jalwa.id }
-output "ipv4_address" { value = digitalocean_droplet.jalwa.ipv4_address }
-output "ipv6_address" { value = digitalocean_droplet.jalwa.ipv6_address }
-output "ssh_user" { value = "jalwa" }
-output "ssh_command" { value = "ssh jalwa@${digitalocean_droplet.jalwa.ipv4_address}" }
+output "droplet_id" {
+  value = digitalocean_droplet.jalwa.id
+}
+
+output "ipv4_address" {
+  value = digitalocean_droplet.jalwa.ipv4_address
+}
+
+output "ipv6_address" {
+  value = digitalocean_droplet.jalwa.ipv6_address
+}
+
+output "ssh_user" {
+  value = "jalwa"
+}
+
+output "ssh_command" {
+  value = "ssh jalwa@${digitalocean_droplet.jalwa.ipv4_address}"
+}
