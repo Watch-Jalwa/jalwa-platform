2e8c9750c9e6c44e81a28bd7ac015b1dc161996f
waseem99 <waseem99@users.noreply.github.com>
2026-07-30 20:16:12 +0500

    Expose DigitalOcean deployment outputs

---
+++ infrastructure/digitalocean/outputs.tf
@@ -0,0 +1,5 @@
+output "droplet_id" { value = digitalocean_droplet.jalwa.id }
+output "ipv4_address" { value = digitalocean_droplet.jalwa.ipv4_address }
+output "ipv6_address" { value = digitalocean_droplet.jalwa.ipv6_address }
+output "ssh_user" { value = "jalwa" }
+output "ssh_command" { value = "ssh jalwa@${digitalocean_droplet.jalwa.ipv4_address}" }
